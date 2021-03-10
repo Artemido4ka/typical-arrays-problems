44eb5be12f328b992592b28721e348b95592db26
Artemido4ka <korenko.artem9@gmail.com>
2021-03-10 17:16:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+    let minimal = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < minimal) {
+        minimal = array[i];
+      }
+    }
+  return minimal;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+  let summ = 0;
+  let result = 0;
+  for(let i=0;i<array.length;i++){
+    summ += array[i];
+  }
+  result = summ/array.length;
+  return result;
 }
